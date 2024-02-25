7bbe91c9afa309a8de0e51feec87d3c81d645959
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2024-02-25 12:30:58 -0800

    Add skeleton page for settings page

--- src/pages/settings/Page.tsx
+++ src/pages/settings/Page.tsx
@@ -1,7 +1,13 @@
 
 const SettingsPage = () => {
   return (
-    <div>SettingsPage</div>
+    <div className="flex flex-col w-full">
+      <div className="header">
+          <h1 className="h1-semibold">Settings</h1>
+      </div>
+      <div className="w-full">
+      </div>
+    </div>
   )
 }
 
